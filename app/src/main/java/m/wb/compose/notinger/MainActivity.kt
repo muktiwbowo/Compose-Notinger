22d2de791e033f6e3f42dfb274e1dc5fd77ea27b
Mukti Wibowo <muktiwbowo@gmail.com>
2022-10-16 21:48:18 +0700

    try to create ui launcher with image and font

--- app/src/main/java/m/wb/compose/notinger/MainActivity.kt
+++ app/src/main/java/m/wb/compose/notinger/MainActivity.kt
@@ -1,15 +1,26 @@
 package m.wb.compose.notinger
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import m.wb.compose.notinger.ui.theme.NotingerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Launcher()
                 }
             }
         }
@@ -30,14 +41,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Launcher() {
+    val context = LocalContext.current
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.padding(horizontal = 32.dp, vertical = 72.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Keep Your Important Notes",
+            fontFamily = FontFamily(Font(R.font.poppins_bold)),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
+        )
+        Image(painter = painterResource(id = R.drawable.image_note), contentDescription = "Logo")
+        Button(onClick = {
+            Toast.makeText(context, "This is a Sample Toast", Toast.LENGTH_LONG).show()
+        }, contentPadding = PaddingValues(horizontal = 72.dp, vertical = 12.dp)) {
+            Text(
+                text = "Write Your Notes",
+                fontFamily = FontFamily(Font(R.font.poppins_bold)),
+                fontSize = 14.sp
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     NotingerTheme {
-        Greeting("Android")
+        Launcher()
     }
 }
